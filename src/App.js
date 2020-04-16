3251737afff460784573fa965c24fce3113d48b2
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-04-16 14:11:58 -0700

    Completed MVP by refactoring the application and extending the functionality with Context API. Also completed the Stretch by creating a functional removeItem function to the application

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -13,20 +15,33 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([
+			...cart,
+			item
+		])
+	};
+
+	const removeItem = id => {
+		setCart([
+			...cart.filter(item => item.id !== id)
+		])
 	};
 
 	return (
+		
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+			
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{cart, removeItem}}>
+					<Navigation />
+					<Route exact path="/">
+						<Products />
+					</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
